f986d7fd622b3736a157c1662ef27a5ac394ab05
Chris <cacarpe1@asu.edu>
2015-02-03 19:44:40 -0700

    Cleaning up LoginScreen

--- asthma/src/LoginScreen.java
+++ asthma/src/LoginScreen.java
@@ -1,4 +1,7 @@
-
+/*
+ * LoginScreen.java
+ * displays and runs elements for the login screen
+ */
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Arrays;
@@ -13,107 +16,89 @@ import javax.swing.JTextField;
 
 public class LoginScreen extends Screen
 {
-	String username;
-	char[] password;
+	//Variables
+	private String testUser = "test";
+	private char[] testPass = {'p','a','s','s'};
 	
-	String testUser = "test";
-	char[] testPass = {'p','a','s','s'};
+	private int loginButtonPressed = 0; //0 is none, 1 is login
 	
-	int butPressed = 0; //0 is none, 1 is login
+	private JTextField userNameTF;
+	private JPasswordField passwordTF;
+	private JPanel loginPanel;
 	
-	JTextField txtf1;
-	JPasswordField txtf2;
-	JPanel pnl;
-	
-	public LoginScreen(Runner run) {
+	public LoginScreen(Runner run) 
+	{
 		super(run);
 		run.frame.setTitle("Login");
 		run.frame.setSize(run.SCR_WIDTH, run.SCR_HEIGHT);
 		run.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		pnl = new JPanel();
+		loginPanel = new JPanel();
 		
-		JLabel lbl1 = new JLabel("Username");
-		lbl1.setSize(100, 20);
-		lbl1.setLocation(200, 200);
+		JLabel userNameLabel = new JLabel("Username");
+		userNameLabel.setSize(100, 20);
+		userNameLabel.setLocation(200, 200);
 		
-		JLabel lbl2 = new JLabel("Password");
-		lbl2.setSize(100, 20);
-		lbl2.setLocation(200, 240);
+		JLabel passwordLabel = new JLabel("Password");
+		passwordLabel.setSize(100, 20);
+		passwordLabel.setLocation(200, 240);
 		
-		txtf1 = new JTextField();
-		txtf1.setSize(100, 20);
-		txtf1.setLocation(200, 220);
+		userNameTF = new JTextField();
+		userNameTF.setSize(100, 20);
+		userNameTF.setLocation(200, 220);
 		
 		
-		txtf2 = new JPasswordField();
-		txtf2.setSize(100, 20);
-		txtf2.setLocation(200, 260);
+		passwordTF = new JPasswordField();
+		passwordTF.setSize(100, 20);
+		passwordTF.setLocation(200, 260);
 		
-		JButton login = new JButton("Login");
-		login.setSize(75, 20);
-		login.setLocation(225, 280);
+		JButton loginButton = new JButton("Login");
+		loginButton.setSize(75, 20);
+		loginButton.setLocation(225, 280);
 		
-		login.addActionListener(new ActionListener()
+		loginButton.addActionListener(new ActionListener()
 		{
+			
 			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				butPressed = 1;				
+			public void actionPerformed(ActionEvent e) 
+			{
+				checkLogin();
+				//loginButtonPressed = 1;				
 			}
 		});
 		
-		pnl.add(lbl1);
-		pnl.add(lbl2);
-		pnl.add(txtf1);
-		pnl.add(txtf2);
-		pnl.add(login);
-		pnl.setLayout(null);		
-		run.frame.setContentPane(pnl);
+		loginPanel.add(userNameLabel);
+		loginPanel.add(passwordLabel);
+		loginPanel.add(userNameTF);
+		loginPanel.add(passwordTF);
+		loginPanel.add(loginButton);
+		loginPanel.setLayout(null);		
+		run.frame.setContentPane(loginPanel);
 		run.frame.setVisible(true);
 	}
 
 	@Override
 	public void update(float deltaTime)
 	{
-		if(butPressed == 1)
-		{
-			username = txtf1.getText();
-			password = txtf2.getPassword();
-
-			if(username.equals(testUser) && Arrays.equals(password, testPass))
-			{
-				run.setScreen(new TutorialScreen(run));
-			}
-			else
-			{
-				JLabel err = new JLabel("Incorrect User/Password");
-				err.setSize(150, 20);
-				err.setLocation(200, 180);
-				pnl.add(err);
-				run.frame.repaint();
-			}
-			
-		}
-		
-		butPressed = 0;
+		//checkLogin();
 	}
 
 	@Override
-	public void present(float deltaTime) {
+	public void present(float deltaTime) 
+	{
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
-	public void pause() {
+	public void pause() 
+	{
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
-	public void resume() {
+	public void resume() 
+	{
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
@@ -121,5 +106,21 @@ public class LoginScreen extends Screen
 	{
 		
 	}
-
+	
+	//Private Methods
+	public void checkLogin()
+	{
+		if(userNameTF.equals(testUser) && Arrays.equals(passwordTF.getPassword(), testPass))
+		{
+			run.setScreen(new TutorialScreen(run));
+		}
+		else
+		{
+			JLabel loginErrorMessage = new JLabel("Incorrect Username/Password");
+			loginErrorMessage.setSize(150, 20);
+			loginErrorMessage.setLocation(200, 180);
+			loginPanel.add(loginErrorMessage);
+			run.frame.repaint();
+		}
+	}
 }
