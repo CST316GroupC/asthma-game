6ec59362e36f9853676e2f93813d8d1861f3a7af
PhenomX <vingateway@hotmail.com>
2015-03-29 20:32:44 -0700

    Added tutorial functionality but would require some fixes

--- asthma/src/com/groupc/screens/TutorialScreen.java
+++ asthma/src/com/groupc/screens/TutorialScreen.java
@@ -17,13 +17,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JToggleButton;
 import javax.swing.SwingConstants;
 
 import com.groupc.Runner;
@@ -36,8 +38,8 @@ public class TutorialScreen extends Screen
 	boolean redraw				= true;
 	boolean played				= true;
 	int 	butPressed			= 0;	
+	int 	step				= 0;
 	Resize  resize				= new Resize(run);
-	
 	
 	//Display Elements
 	NavigationBar navBar		= new NavigationBar(run,false,false,"Tutorial");
@@ -54,9 +56,27 @@ public class TutorialScreen extends Screen
 	JButton step3				= new JButton("Step 3");
 	JButton step4				= new JButton("Step 4");
 	JButton step5				= new JButton("Step 5");
+	JButton step6				= new JButton("Step 6");
+	JButton step7				= new JButton("Step 7");
 	JButton start				= new JButton("Start");
 	
-	public TutorialScreen(Runner run) 
+	ImageIcon image1 			= new ImageIcon("tutorial1.png");
+	ImageIcon image2 			= new ImageIcon("tutorial2.png");
+	ImageIcon image3 			= new ImageIcon("tutorial3.png");
+	ImageIcon image4 			= new ImageIcon("tutorial4.png");
+	ImageIcon image5 			= new ImageIcon("tutorial5.png");
+	ImageIcon image6 			= new ImageIcon("tutorial6.png");
+	ImageIcon image7 			= new ImageIcon("tutorial7.png");
+	
+	JLabel step1Image			= new JLabel(image1);
+	JLabel step2Image			= new JLabel(image2);
+	JLabel step3Image			= new JLabel(image3);
+	JLabel step4Image			= new JLabel(image4);
+	JLabel step5Image			= new JLabel(image5);
+	JLabel step6Image			= new JLabel(image6);
+	JLabel step7Image			= new JLabel(image7);
+	
+	public TutorialScreen(Runner run) throws IOException
 	{
 		super(run);
 		
@@ -78,56 +98,98 @@ public class TutorialScreen extends Screen
 		descriptionBox.setBackground(Color.LIGHT_GRAY);
 		stepsBox.setBackground(Color.GRAY);
 		skipText.setForeground(Color.RED);
-		
-		//Font 
-		videoBox.setBounds(30, 120, 250, 180);
-		descriptionBox.setBounds(30, 320, 250, 60);
-		stepsBox.setBounds(310, 120, 150, 260);
-		
-		
-		// Add Steps button
-		step1 = new JButton("Step 1");
-		step2 = new JButton("Step 2");
-		step3 = new JButton("Step 3");
-		step4 = new JButton("Step 4");
-		step5 = new JButton("Step 5");
-	
-		step1.setBounds(345, 140, 80, 25);
-		step2.setBounds(345, 190, 80, 25);
-		step3.setBounds(345, 240, 80, 25);
-		step4.setBounds(345, 290, 80, 25);
-		step5.setBounds(345, 340, 80, 25);
 
-		start.setBounds(200, 420, 80, 25 );
-		
-		skipText.setBounds(180, 395, 120, 25);
 		
 		
 		////Buttons////
 		start.addActionListener(new ActionListener()
 		{
 			@Override
-			public void actionPerformed(ActionEvent arg0) {
+			public void actionPerformed(ActionEvent arg0) 
+			{
 				butPressed = 1;
 			}
 		});
-
-//		// Add start button listener
-//		start.addActionListener(new ActionListener()
-//		{
-//			@Override
-//			public void actionPerformed(ActionEvent arg0) {
-//				butPressed = 4;
-//			}
-//		});
 		
+		step1.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				step = 1;
+			}
+		});
+				
+
+		step2.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				step = 2;
+			}
+		});
+		
+		step3.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				step = 3;
+			}
+		});
+		
+		step4.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				step = 4;
+			}
+		});
+		
+		step5.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				step = 5;
+			}
+		});
+		step6.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				step = 6;
+			}
+		});
+		
+		step7.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				step = 7;
+			}
+		});
+								
 		this.add(skipText);
 		this.add(step1);
 		this.add(step2);
 		this.add(step3);
 		this.add(step4);
 		this.add(step5);
+		this.add(step6);
+		this.add(step7);
 		this.add(start);
+		this.add(step1Image);
+		this.add(step2Image);
+		this.add(step3Image);
+		this.add(step4Image);
+		this.add(step5Image);
+		this.add(step6Image);
+		this.add(step7Image);
 		
 		// Panels have to be added last for it to show 
 		this.add(videoBox);
@@ -137,6 +199,7 @@ public class TutorialScreen extends Screen
 		this.setLayout(null);		
 		run.setContentPane(this);
 		run.setVisible(true);
+		stepVisibility();
 	}
 
 	@Override
@@ -153,20 +216,29 @@ public class TutorialScreen extends Screen
 			//description
 			descriptionBox.setBounds(resize.locationX(30), resize.locationY(320), resize.width(250), resize.height(60));
 			descriptionBox.setBorder(BorderFactory.createLineBorder(Color.black, resize.height(1)));
+
 			
 			//step
-			stepsBox.setBounds(resize.locationX(310), resize.locationY(120), resize.width(150), resize.height(260));
+			stepsBox.setBounds(resize.locationX(330), resize.locationY(120), resize.width(150), resize.height(260));
 			stepsBox.setBorder(BorderFactory.createLineBorder(Color.black, resize.height(1)));
-			step1.setBounds(resize.locationX(345), resize.locationY(140), resize.width(80), resize.height(25));
+			step1.setBounds(resize.locationX(370), resize.locationY(135), resize.width(75), resize.height(20));
 			step1.setFont(new Font(step1.getFont().getFontName(),step1.getFont().getStyle(), resize.font(12)));
-			step2.setBounds(resize.locationX(345), resize.locationY(190), resize.width(80), resize.height(25));
+			step2.setBounds(resize.locationX(370), resize.locationY(170), resize.width(75), resize.height(20));
 			step2.setFont(new Font(step2.getFont().getFontName(),step2.getFont().getStyle(), resize.font(12)));
-			step3.setBounds(resize.locationX(345), resize.locationY(240), resize.width(80), resize.height(25));
+			step3.setBounds(resize.locationX(370), resize.locationY(205), resize.width(75), resize.height(20));
 			step3.setFont(new Font(step3.getFont().getFontName(),step3.getFont().getStyle(), resize.font(12)));
-			step4.setBounds(resize.locationX(345), resize.locationY(290), resize.width(80), resize.height(25));
+			step4.setBounds(resize.locationX(370), resize.locationY(240), resize.width(75), resize.height(20));
 			step4.setFont(new Font(step4.getFont().getFontName(),step4.getFont().getStyle(), resize.font(12)));
-			step5.setBounds(resize.locationX(345), resize.locationY(340), resize.width(80), resize.height(25));
+			step5.setBounds(resize.locationX(370), resize.locationY(275), resize.width(75), resize.height(20));
 			step5.setFont(new Font(step5.getFont().getFontName(),step5.getFont().getStyle(), resize.font(12)));
+			step6.setBounds(resize.locationX(370), resize.locationY(310), resize.width(75), resize.height(20));
+			step6.setFont(new Font(step6.getFont().getFontName(),step6.getFont().getStyle(), resize.font(12)));
+			step7.setBounds(resize.locationX(370), resize.locationY(345), resize.width(75), resize.height(20));
+			step7.setFont(new Font(step7.getFont().getFontName(),step7.getFont().getStyle(), resize.font(12)));
+			
+			//image
+			step1Image.setBounds(resize.locationX(30), resize.locationY(120), resize.width(250), resize.height(180));
+			
 			
 			//skipText
 			skipText.setBounds(resize.locationX(0), resize.locationY(395), resize.width(500), resize.height(25));
@@ -197,6 +269,56 @@ public class TutorialScreen extends Screen
 				played = true;
 			}
 		}
+		
+		if(step == 1)
+		{
+			stepVisibility();
+			videoBox.setVisible(false);
+			step1Image.setVisible(true);
+			step = 0;
+		}
+		else if(step == 2)
+		{
+			stepVisibility();
+			videoBox.setVisible(false);
+			step2Image.setVisible(true);
+			step = 0;
+		}
+		else if(step == 3)
+		{
+			stepVisibility();
+			videoBox.setVisible(false);
+			step3Image.setVisible(true);
+			step = 0;
+		}
+		else if(step == 4)
+		{
+			stepVisibility();
+			videoBox.setVisible(false);
+			step4Image.setVisible(true);
+			step = 0;
+		}
+		else if(step == 5)
+		{
+			stepVisibility();
+			videoBox.setVisible(false);
+			step5Image.setVisible(true);
+			step = 0;
+		}
+		else if(step == 6)
+		{
+			stepVisibility();
+			videoBox.setVisible(false);
+			step6Image.setVisible(true);
+			step = 0;
+		}
+		else if(step == 7)
+		{
+			stepVisibility();
+			videoBox.setVisible(false);
+			step7Image.setVisible(true);
+			step = 0;
+		}
 	}
 
 	@Override
@@ -224,6 +346,17 @@ public class TutorialScreen extends Screen
 	public void dispose() 
 	{
 		// TODO Auto-generated method stub
+	}
+	
+	public void stepVisibility()
+	{
+		step1Image.setVisible(false);
+		step2Image.setVisible(false);
+		step3Image.setVisible(false);
+		step4Image.setVisible(false);
+		step5Image.setVisible(false);
+		step6Image.setVisible(false);
+		step7Image.setVisible(false);
 	}
 
 }
